b3caaca6a4390ed6e5d0cfee2f598776eddfca15
Jacob Long <jlongrc@gmail.com>
2017-08-29 00:45:56 -0400

    Updates to wgttest for glm support, improved docs and output

--- R/wgttest.R
+++ R/wgttest.R
@@ -5,15 +5,21 @@
 #'
 #' @param model The unweighted linear model (must be \code{lm},
 #' \code{glm}, see details for other types) you want to check.
+#'
+#' @param data The data frame with the data fed to the fitted model and the weights
+#'
 #' @param weights The name of the weights column in \code{model}'s data frame
 #'   or a vector of weights equal in length to the number of observations
 #'   included in \code{model}.
+#'
 #' @param model_output Should a summary of the model with weights as predictor
 #'   be printed? Default is TRUE, but you may not want it if you are trying to
 #'   declutter a document.
+#'
 #' @param test Which type of test should be used in the ANOVA? The default,
 #'   \code{NULL}, chooses based on the model type ("F" for linear models).
 #'   This argument is passed to \code{anova}.
+#'
 #' @param digits An integer specifying the number of digits past the decimal to
 #'   report in the output. Default is 3. You can change the default number of
 #'   digits for all jtools functions with
@@ -33,8 +39,10 @@
 #' It can be helpful to look at the created model using \code{model_output = TRUE}
 #' to see which variables might be the ones affected by inclusion of weights.
 #'
-#' This test can support most GLMs in addition to LMs. It does not work for
-#' mixed models (e.g., \code{lmer} or \code{lme}) though it could plausibly be
+#' This test can support most GLMs in addition to LMs, a use validated by
+#' Nordberg (1989). This, to my knowledge, is different from the Stata macro.
+#' It does not work for mixed models (e.g., \code{lmer} or \code{lme}) though
+#' it could plausibly be
 #' implemented. However, there is no scholarly consensus how to properly
 #' incorporate weights into mixed models. There are other types of models that
 #' may work, but have not been tested. The function is designed to be
@@ -42,6 +50,8 @@
 #' careful to make sure s/he understands whether this type of test is
 #' appropriate for the model being considered. DuMouchel and Duncan (1983) were
 #' only thinking about linear regression when the test was conceived.
+#' Nordberg (1989) validated its use with generalized linear models, but to
+#' this author's knowledge it has not been tested with other model types.
 #'
 #' @references
 #'
@@ -49,9 +59,13 @@
 #'   multiple regression analyses of stratified samples. \emph{Journal of the
 #'   American Statistical Association}, \emph{78}. 535-543.
 #'
+#' Nordberg, L. (1989). Generalized linear modeling of sample survey data.
+#'   \emph{Journal of Official Statistics; Stockholm}, \emph{5}, 223–239.
+#'
 #' Winship, C. & Radbill, L. (1994). Sampling weights and regression
 #'   analysis. \emph{Sociological Methods and Research}, \emph{23}, 230-257.
 #'
+#' @family survey tools
 #'
 #' @examples
 #' # First, let's create some fake sampling weights
@@ -68,6 +82,8 @@
 #' y <- rbinom(100, 1, .5)
 #' fit <- glm(y ~ x, family = binomial)
 #' wgttest(fit, wts)
+#' ## Can specify test manually
+#' wgttest(fit, wts, test = "Rao")
 #'
 #' # Quasi family is treated differently than likelihood-based
 #' ## Dobson (1990) Page 93: Randomized Controlled Trial (plus some extra values):
@@ -81,19 +97,48 @@
 #' @importFrom stats anova reformulate
 #' @export
 
-wgttest <- function(model, weights, model_output = TRUE, test = NULL,
+wgttest <- function(model, data = NULL, weights, model_output = TRUE,
+                    test = NULL,
                     digits = getOption("jtools-digits", default = 3)) {
 
   # Need to parse the arguments
-  wname <- as.character(substitute(weights))
-
-  call <- getCall(model)
-  if (!is.null(call$data)) {
-    d <- eval(call$data)
-    mframe <- FALSE # telling myself whether I use model.frame
+  # Need to parse the arguments
+  if (length(as.character(substitute(weights))) == 1 &&
+      as.character(substitute(weights)) %in% names(data)) {
+    wname <- as.character(substitute(weights))
+    numeric.input <- FALSE
   } else {
-    d <- model.frame(model)
-    mframe <- TRUE
+    if (is.numeric(weights)) {
+      numeric.input <- TRUE
+    } else {
+      stop("weights argument must be either the name of a column in the data frame or a numeric vector.")
+    }
+  }
+
+  if (!is.null(data)) {
+
+    call <- getCall(model)
+    if (!is.null(call$data)) {
+
+      d <- eval(call$data)
+
+    } else {
+
+      d <- model.frame(model)
+
+    }
+
+  } else {
+
+    d <- data
+
+  }
+
+  # Save to d.f. and handle weights entered as numeric vector
+  d <- as.data.frame(d)
+  if (numeric.input == TRUE) {
+    d$weight <- weights
+    wname <- "weight"
   }
 
   # Save response variable
@@ -179,13 +224,15 @@ wgttest <- function(model, weights, model_output = TRUE, test = NULL,
 
 }
 
-#### PRINT FUNCTION
+#######################################################################
+#  PRINT METHOD                                                       #
+#######################################################################
 
 #' @export
 
 print.wgttest <- function(x, ...) {
 
-  cat("Omnibus test of model change with weights\n")
+  cat("DuMouchel-Duncan test of model change with weights\n\n")
 
   if (attr(x, "linear") == TRUE && attr(x, "lm") == TRUE &&
       attr(x, "test") == "F") {
@@ -221,6 +268,14 @@ print.wgttest <- function(x, ...) {
 
   if (x$model_output == TRUE) {
     j_summ(x$newmod, model.info = F, model.fit = F)
+    cat("\n")
+  } else {
+    cat("\n")
   }
 
 }
+#'
+  }
+
+}
+#'
