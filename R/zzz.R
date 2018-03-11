248544b44878af3b431a97675ece628ab29254ba
Jacob Long <jlongrc@gmail.com>
2018-03-11 13:11:19 -0400

    Better package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,20 @@
 # Pkg startup message
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
 
-  if (interactive() & getOption("jtools-update-msg", FALSE) == FALSE) {
-    packageStartupMessage("The most recent jtools update (to 1.0.0) was a major",
-              " update.\nPlease check out",
-              " http://www.jtools.jacob-long.com/news/\n",
-              "for details on what is new.\n")
-  }
+  # Thanks to G. Grothendieck for this idea for giving a limited time
+  # message after installation
+  # https://stackoverflow.com/questions/40748744/r-package-with-code-that-only
+  # -runs-once-per-installation
 
   if (interactive()) {
-    options("jtools-update-msg" = TRUE)
+    ctime <- file.info(find.package(pkgname))$ctime
+    days <- as.integer(round(difftime(Sys.time(), ctime, units = "day"), 0))
+  } else {days <- 4}
+  if (days < 3) {
+    msg <- wrap_str("The most recent jtools update (to 1.0.0) was a major
+    update. Please check out http://www.jtools.jacob-long.com/news/ for
+    details on what is new. This message will go away in ", 3 - days, " days.")
+    packageStartupMessage(msg)
   }
+
 }
