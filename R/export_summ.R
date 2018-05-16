7be3e2b339cdd678b4e2998c7619492d38f134c7
Jacob Long <jlongrc@gmail.com>
2018-05-16 11:28:19 -0400

    R <3.3 compatibility fix w/ getS3method
    Addresses #28

--- R/export_summ.R
+++ R/export_summ.R
@@ -667,8 +667,10 @@ make_tidies <- function(mods, ex_args, ci_level, model.names, omit.coefs,
     if (!is.null(ex_args)) {
 
       method_stub <- find_S3_class("tidy", mods[[i]], package = "broom")
-      method_args <-
-        formals(getS3method("tidy", method_stub, envir = getNamespace("broom")))
+      # getS3method() only available in R >= 3.3
+      the_method <- get(paste0("tidy.", method_stub), asNamespace("broom"),
+                        mode = "function")
+      method_args <- formals(the_method)
 
       method_args <-
         method_args[names(method_args) %nin% c("intervals", "prob")]
