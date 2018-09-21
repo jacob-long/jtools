jacob-long/jtools

commit 648a97083d3b4095a380d307a190ae282ff5fca7

Fix dropping of non-numeric covariates in plotters
Closes #41

R/int_utils.R
@@ -518,8 +518,13 @@ center_values_non_survey <- function(d, weights, omitvars, centered) {
       centered <- centered[centered %nin% omitvars]
     }
     if (length(centered) > 0) {
-      vals <- sapply(d[centered], weighted.mean, weights = weights,
-                     na.rm = TRUE)
+      vals <- sapply(d[centered], function(x, weights, na.rm) {
+        if (is.numeric(x)) {
+          return(weighted.mean(x, weights, na.rm = na.rm))
+        } else {
+          return(levels(factor(x))[1])
+        }
+      }, weights = weights, na.rm = TRUE)
     }
 
   } else if (centered == "all") {
@@ -528,7 +533,13 @@ center_values_non_survey <- function(d, weights, omitvars, centered) {
     vars <- names(d)[names(d) %nin% omitvars]
 
     if (length(vars) > 0) {
-      vals <- sapply(d[vars], weighted.mean, weights = weights)
+      vals <- sapply(d[vars], function(x, weights, na.rm) {
+        if (is.numeric(x)) {
+          return(weighted.mean(x, weights, na.rm = na.rm))
+        } else {
+          return(levels(factor(x))[1])
+        }
+      }, weights = weights, na.rm = TRUE)
     }
 
   } else if (centered == "none") {
@@ -562,7 +573,20 @@ center_values_survey <- function(d, omitvars, design = NULL,
     }
 
     if (length(centered) > 0) {
-      vals <- survey::svymean(survey::make.formula(centered), design)
+      # Pulling out non-numeric vars
+      facvars <- sapply(d[centered], is.numeric)
+      facvars <- names(facvars[facvars == FALSE])
+      if (length(facvars) > 0) {centered <- centered[centered %nin% facvars]}
+      
+      if (length(centered) > 0) { # might have just pulled out all non-focals
+        vals <- survey::svymean(survey::make.formula(centered), design)
+      } else {vals <- NULL}
+      
+      if (length(facvars) > 0) { # return ref level of non-numerics
+        vals <- c(vals, sapply(d[facvars], function(x) {
+          return(levels(factor(x))[1])
+        }))
+      }
     }
     d <- design$variables
 
@@ -571,7 +595,19 @@ center_values_survey <- function(d, omitvars, design = NULL,
     vars <- names(d)[names(d) %nin% omitvars]
 
     if (length(vars) > 0) {
-      vals <- survey::svymean(survey::make.formula(vars), design)
+      facvars <- sapply(d[vars], is.numeric)
+      facvars <- names(facvars[facvars == FALSE])
+      if (length(facvars) > 0) {vars <- vars[vars %nin% facvars]}
+      
+      if (length(vars) > 0) { # might have just pulled out all non-focals
+        vals <- survey::svymean(survey::make.formula(vars), design)
+      } else {vals <- NULL}
+      
+      if (length(facvars) > 0) { # return ref level of non-numerics
+        vals <- c(vals, sapply(d[facvars], function(x) {
+          return(levels(factor(x))[1])
+        }))
+      }
     }
 
   } else if (centered == "none") {
@@ -776,7 +812,8 @@ prep_data <- function(model, d, pred, modx, mod2, pred.values = NULL,
     facvars[facvars %nin% c(pred, resp, modx, mod2, wname, offname)]
 
   # Create omitvars variable; we don't center any of these
-  omitvars <- c(pred, resp, modx, mod2, wname, offname, facvars,
+  omitvars <- c(pred, resp, modx, mod2, wname, offname, 
+                # facvars,
                 "(weights)", "(offset)")
 
   if (survey == FALSE) {

commit 6809ff88341a06fec78220d998d44b1696d5d20f

Fix new bug introduced in 648a970
By returning a vector of mean values, items were coerced to character when the context was of mixed type. Now I use a list (lapply)

R/int_utils.R
@@ -518,7 +518,7 @@ center_values_non_survey <- function(d, weights, omitvars, centered) {
       centered <- centered[centered %nin% omitvars]
     }
     if (length(centered) > 0) {
-      vals <- sapply(d[centered], function(x, weights, na.rm) {
+      vals <- lapply(d[centered], function(x, weights, na.rm) {
         if (is.numeric(x)) {
           return(weighted.mean(x, weights, na.rm = na.rm))
         } else {
@@ -533,7 +533,7 @@ center_values_non_survey <- function(d, weights, omitvars, centered) {
     vars <- names(d)[names(d) %nin% omitvars]
 
     if (length(vars) > 0) {
-      vals <- sapply(d[vars], function(x, weights, na.rm) {
+      vals <- lapply(d[vars], function(x, weights, na.rm) {
         if (is.numeric(x)) {
           return(weighted.mean(x, weights, na.rm = na.rm))
         } else {
@@ -583,7 +583,7 @@ center_values_survey <- function(d, omitvars, design = NULL,
       } else {vals <- NULL}
       
       if (length(facvars) > 0) { # return ref level of non-numerics
-        vals <- c(vals, sapply(d[facvars], function(x) {
+        vals <- c(vals, lapply(d[facvars], function(x) {
           return(levels(factor(x))[1])
         }))
       }
@@ -604,7 +604,7 @@ center_values_survey <- function(d, omitvars, design = NULL,
       } else {vals <- NULL}
       
       if (length(facvars) > 0) { # return ref level of non-numerics
-        vals <- c(vals, sapply(d[facvars], function(x) {
+        vals <- c(vals, lapply(d[facvars], function(x) {
           return(levels(factor(x))[1])
         }))
       }
@@ -1123,7 +1123,7 @@ make_pred_frame_cont <- function(d, pred, modx, modxvals2, mod2, mod2vals2,
   if (!is.null(vals)) {
     vals <- vals[names(vals) %nin% c(offname,modx,mod2,pred,resp)]
     for (var in names(vals)) {
-      pm[[var]] <- rep(vals[var], times = nrow(pm))
+      pm[[var]] <- rep(vals[[var]], times = nrow(pm))
     }
   }
 
@@ -1215,7 +1215,7 @@ make_pred_frame_cat <- function(d, pred,
   if (!is.null(vals)) {
     vals <- vals[names(vals) %nin% c(offname, modx, mod2, pred, resp)]
     for (var in names(vals)) {
-      pm[[var]] <- rep(vals[var], times = nrow(pm))
+      pm[[var]] <- rep(vals[[var]], times = nrow(pm))
     }
   }
 